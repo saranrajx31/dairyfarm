e31c675dd75753d2fa779d4a49f99c2c9f027c79
saranrajx31 <saranrajx31@users.noreply.github.com>
2026-07-21 21:15:59 +0530

    allow_render_hosts_in_vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: true,
+  },
+  server: {
+    allowedHosts: true,
+  },
 })
-
